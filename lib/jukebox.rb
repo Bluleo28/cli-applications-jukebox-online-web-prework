2b2c260a2569da8cedb1d1c7c85e7a48d794b74a
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 20:52:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,4 +3,3 @@ expect { play(songs) } .to output(/Phoenix - 1901/) .to_stdout
 expect { play(songs) } .to_output(/Phoenix - 1901/) .to_stdout
 expect { play(songs) } .to output(/Invalid input, please try again/) .to_stdout
 expect { list(songs) } .to output(/1. Phoenix - 1901/) .to_stdout
-
